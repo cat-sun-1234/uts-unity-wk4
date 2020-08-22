f1c77be574150d0b4a1eaecdb5628c10ae4bad4d
cat-sun-1234 <45275829+cat-sun-1234@users.noreply.github.com>
2020-08-22 17:08:12 +1000

    80% push

---
+++ Assets/FeaturePrint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeaturePrint : MonoBehaviour
+{
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(this.gameObject.name + ": " + i);
+    }
+}
