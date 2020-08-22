caed51dba2237afedd9c10af2b7e825d4e8f50ca
cat-sun-1234 <45275829+cat-sun-1234@users.noreply.github.com>
2020-08-22 17:14:28 +1000

    90% push

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+
     [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FeaturePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     int i = 0;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
